e70661823dff5c3ae57621f09868f1fc8cfd341c
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-19 14:43:11 +0900

    index.jsへ処理の実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import "./styles.css";
+const onClickAdd = () => {
+  //テキストボックスのElementの取得
+  const textEl = document.getElementById("add-text");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  //テキストボックスの値を取得
+  const text = textEl.value;
+
+  //テキストボックスを初期化
+  textEl.value = "";
+
+  //liタグの生成
+  const li = document.createElement("li");
+
+  //divタグの生成
+  const div = document.createElement("div");
+
+  //pタグを生成
+  const p = document.createElement("p");
+  p.textContent = text;
+
+  //buttonタグの生成
+  const button = document.createElement("button");
+  button.textContent = "削除";
+
+  //ボタンを押したときに行を削除する処理
+  button.addEventListener("click", () => {
+    const deleteTarget = button.closest("li");
+
+    document.getElementById("memo-list").removeChild(deleteTarget);
+  });
+
+  //divタグ配下にpタグとbuttonタグの設定
+  div.appendChild(p);
+  div.appendChild(button);
+
+  //liタグ配下に上記のdivタグを設定
+  li.appendChild(div);
+
+  //メモ一覧リストに上記のliタグを設定
+  document.getElementById("memo-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
